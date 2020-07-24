1a0e219cacbb768bc261e0b23d237fcce622696f
Vishal_Bait <vishalbait01@gmail.com>
2020-07-24 11:33:05 +0530

    Add file programs.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -27,10 +27,19 @@
         </div>
       </li>
 
-
-      <li class="nav-item">
-        <a class="nav-link text-white font-weight-bold  mx-2" href="#">ACADEMICS</a>
+       <li class="nav-item dropdown">
+        <a class="nav-link text-white font-weight-bold  mx-2 dropdown-toggle" href="#" id="navbarDropdown"
+        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          ACADEMICS
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="programs.php">Programs Offered</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </li>
+
 
        <li class="nav-item">
         <a class="nav-link text-white font-weight-bold  mx-2" href="#">PLACEMENT</a>
---
+++ programs.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viva Institute</title>
+
+    <!-- Include Header Scripts -->
+    <?php include_once "includes/headerScripts.php";?>
+</head>
+
+<body>
+
+    <!-- Include Navbar -->
+    <?php include_once "includes/navbar.php";?>
+
+
+    <main class="container">
+
+        <h3 class="alert alert-primary  text-uppercase
+        mt-4 text-center text-dark font-time">Programs Offered</h3>
+
+        <div class="row">
+
+            <section class="col-md-10 offset-md-1 my-3">
+
+                <div class="text-center mb-2">
+                    <img src="images/Courses Offered.png" class="img-fluid" alt="Courses Offered">
+                </div>
+
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Program</th>
+                                <th>Qualification</th>
+                                <th>Duration</th>
+                                <th>Fee</th>
+                                <th>Centre</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">Hotel Management (Diploma)</td>
+                                <td>SSC/HSC/Any Graduation</td>
+                                <td>1 Year</td>
+                                <td>&#x20b9; 11500/-</td>
+                                <td>Mangaon</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">Hotel Management (Certificate Courses)</td>
+                                <td>SSC/HSC/Any Graduation</td>
+                                <td>6 Months</td>
+                                <td>&#x20b9; 7500/-</td>
+                                <td>Mangaon</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row">Special Cooking Courses</td>
+                                <td>SSC/HSC/Any Graduation</td>
+                                <td>3 Months</td>
+                                <td>&#x20b9; 7500/-</td>
+                                <td>Mangaon</td>
+                            </tr>
+
+                            <tr>
+                                <td scope="row">Bar Tender Courses</td>
+                                <td>SSC/HSC/Any Graduation</td>
+                                <td>3 Months</td>
+                                <td>&#x20b9; 10000/-</td>
+                                <td>Mangaon</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <hr>
+                <h5 class="font-time text-uppercase text-danger"><i class="fa fa-star-o" aria-hidden="true"></i>
+                    Academic Features</h5>
+                <ul>
+                    <li>Advanced Digital Classroom</li>
+                    <li>Experinced Faculty</li>
+                    <li>Hotel Management Certificate</li>
+                    <li>100% Job Guarantee</li>
+                </ul>
+
+                <hr>
+
+                <h5 class="font-time text-uppercase text-danger"><i class="fa fa-star-o" aria-hidden="true"></i>
+                    Documents
+                    Required for Admission</h5>
+                <ul>
+                    <li>4 Passport Sixe photos</li>
+                    <li>Marklist/Certificate of Higher Education</li>
+                    <li>Leaving Certificate/Birth Certificate</li>
+                    <li>Aadhar Card</li>
+                </ul>
+
+            </section>
+        </div>
+    </main>
+
+
+    <!-- Include Footer -->
+    <?php include_once "includes/footer.php";?>
+    <!-- Include Footer Script -->
+    <?php include_once "includes/footerScripts.php";?>
+
+</body>
+
+</html>
